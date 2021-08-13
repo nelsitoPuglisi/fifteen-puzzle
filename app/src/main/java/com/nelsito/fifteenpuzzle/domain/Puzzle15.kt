c0f58f96fc2f54838b43b17101bb92f726924c11
nelsito <nelson.puglisi@protonmail.com>
2021-08-13 19:21:00 -0300

    refactor

--- app/src/main/java/com/nelsito/fifteenpuzzle/domain/Puzzle15.kt
+++ app/src/main/java/com/nelsito/fifteenpuzzle/domain/Puzzle15.kt
@@ -8,25 +8,26 @@ data class Puzzle15(val tiles: List<Int>) {
     private val size = 4
 
     fun down(): Puzzle15 {
-        //only if blank tile is not at the top row: i.e. index >= size
         val blankIndex = tiles.indexOf(0)
-        if (blankIndex <=3) return Puzzle15(tiles)
+
+        //only if blank tile is not at the top row: i.e. index >= size
+        if (blankIndex <= 3) return Puzzle15(tiles)
         val targetIndex = blankIndex - size
 
-        val newTiles = tiles.toMutableList().apply {
-            this[blankIndex] = tiles[targetIndex]
-            this[targetIndex] = 0
-        }
-        return Puzzle15(newTiles)
+        return moveTiles(blankIndex, targetIndex)
     }
 
     fun right(): Puzzle15 {
-        //only if blank tile is not at the first column: i.e. index % size > 0
         val blankIndex = tiles.indexOf(0)
-        if (blankIndex % size == 0) return Puzzle15(tiles)
 
+        //only if blank tile is not at the first column: i.e. index % size > 0
+        if (blankIndex % size == 0) return Puzzle15(tiles)
         val targetIndex = blankIndex - 1
 
+        return moveTiles(blankIndex, targetIndex)
+    }
+
+    private fun moveTiles(blankIndex: Int, targetIndex: Int): Puzzle15 {
         val newTiles = tiles.toMutableList().apply {
             this[blankIndex] = tiles[targetIndex]
             this[targetIndex] = 0
